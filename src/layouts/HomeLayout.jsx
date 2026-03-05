1cc76820b3cf31d4c2675d201586c8135ba8cff2
areful75 <mrarefulomi@gmail.com>
2026-03-05 20:29:11 +0600

    added sticky behaviour

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -17,16 +17,16 @@ const HomeLayout = () => {
                 <nav className='w-11/12 mx-auto'>
                     <Navbar></Navbar>
                 </nav>
-            </header>
+            </header> 
             <main className='w-11/12 mx-auto my-3  grid grid-cols-12 gap-5'>
                
-               <aside className='col-span-3'>
+               <aside className='col-span-3 sticky h-fit top-0'>
                 <LeftAside></LeftAside>
                </aside>
                 <section className="main col-span-6">
                     <Outlet></Outlet>
                 </section>
-              <aside className='col-span-3'>
+              <aside className='col-span-3 sticky h-fit top-0'>
               <RightAside></RightAside>
               </aside>
             </main>
